6aa716b0941db1f6c774e24c16e5090b86f0898c
Archen Anicure <archenanicure@gmail.com>
2026-01-25 02:09:09 +0300

    Create how_do_i_get_her_off_my_screen.lua

---
+++ how_do_i_get_her_off_my_screen.lua
@@ -0,0 +1,38 @@
+local url = "https://i.redd.it/hituvwjkdee91.png"
+local images = {}
+local count = 0
+local delay = 1
+local mindelay = 0.01
+
+local function SpawnImage()
+    count = count + 1
+    local filename = "cirno" .. count .. ".png"
+
+    http.Fetch(url, function(body)
+        file.Write(filename, body)
+        local mat = Material("../data/" .. filename)
+
+        table.insert(images, {
+            mat = mat,
+            x = math.random(0, ScrW() - 128),
+            y = math.random(0, ScrH() - 128),
+            size = 128
+        })
+        surface.PlaySound("ambient/creatures/teddy.wav")
+    end)
+
+    if count % 1 == 0 then
+        delay = math.max(mindelay, delay - 0.01)
+        timer.Adjust("CirnoBorn", delay, 0, SpawnImage)
+    end
+end
+
+timer.Create("CirnoBorn", delay, 0, SpawnImage)
+
+hook.Add("HUDPaint", "DrawCirno", function()
+    for _, img in ipairs(images) do
+        surface.SetMaterial(img.mat)
+        surface.SetDrawColor(255,255,255)
+        surface.DrawTexturedRect(img.x, img.y, img.size, img.size)
+    end
+end)
